bd7295188b600ab3763a2cf3340f78f39e8e8938
yakul khajuria <yakul@crossml.com>
2021-08-23 17:54:56 +0530

    excced limit add

--- mytask/polls/views.py
+++ mytask/polls/views.py
@@ -20,15 +20,18 @@ def submit(request):
                 health=request.POST.get('health')
                 personal=request.POST.get('personal')
                 fees=request.POST.get('fees')
-                sum=int(travel+eduction+gifts+investments+bills+food+health+personal+fees)
+                # sum=int(travel+eduction+gifts+investments+bills+food+health+personal+fees)
+                sum=int(travel)+int(eduction)+int(gifts)+int(investments)+int(bills)+int(food)+int(health)+int(personal)+int(fees)
                 if sum>5000:
                     print("you cross your montly limit")
                     Table=Expense(travel=travel,eduction=eduction, gifts=gifts, Investments=investments,Bills=bills,Food=food,Health=health,Personal=personal,fee=fees)
                     Table.save()
+                    warning={'key1':'exceed daily limit'}
+                    return render(request, 'submit.html', warning)   
                 else:
                     Table=Expense(travel=travel,eduction=eduction, gifts=gifts, Investments=investments,Bills=bills,Food=food,Health=health,Personal=personal,fee=fees)
                     Table.save()
-        return render(request, 'submit.html')   
+                    return render(request, 'submit.html')   
 
 
 def show(request):
